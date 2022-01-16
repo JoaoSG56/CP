cfcad736ac913ec4e9e7addcb2b64be09964ba55
João Guedes <a89588@alunos.uminho.pt>
2022-01-16 23:28:38 +0000

    fix

--- Sequential/sequential.c
+++ Sequential/sequential.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#include "papi.h"
 
 // PAPI events to monitor
 // #define NUM_EVENTS 4
@@ -25,8 +26,7 @@ struct bucket{
     int tam;
     int topo;   
     int *balde;
-};
-typedef struct bucket *Bucket;
+}*Bucket;
 
 /* 
 nº buckets : pre-definido
@@ -114,7 +114,7 @@ void bubble(int v[],int tam){
 int main(){
     long long start_usec, end_usec, elapsed_usec, min_usec=0L;
     printf("tam_bucket: %d\n",tam_bucket);
-    int i;
+    int i,num_hwcntrs;
 
     int *vetor = (int *) malloc(sizeof(int)*dim);
 
@@ -127,39 +127,39 @@ int main(){
 
 
 
-    // // Calculate the time taken by take_enter()
-    // retval = PAPI_library_init(PAPI_VER_CURRENT);
-    // if (retval != PAPI_VER_CURRENT) {
-    //     fprintf(stderr,"PAPI library init error!\n");
-    //     return 0;
-    // }
+    // Calculate the time taken by take_enter()
+    retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT) {
+        fprintf(stderr,"PAPI library init error!\n");
+        return 0;
+    }
 
-    // /* create event set */
-    // if (PAPI_create_eventset(&EventSet) != PAPI_OK) {
-    //     fprintf(stderr,"PAPI create event set error\n");
-    //     return 0;
-    // }
+    /* create event set */
+    if (PAPI_create_eventset(&EventSet) != PAPI_OK) {
+        fprintf(stderr,"PAPI create event set error\n");
+        return 0;
+    }
 
 
-    // /* Get the number of hardware counters available */
-    // if ((num_hwcntrs = PAPI_num_hwctrs()) <= PAPI_OK)  {
-    //     fprintf (stderr, "PAPI error getting number of available hardware counters!\n");
-    //     return 0;
-    // }
-    // fprintf(stdout, "done!\nThis system has %d available counters.\n\n", num_hwcntrs);
+    /* Get the number of hardware counters available */
+    if ((num_hwcntrs = PAPI_num_hwctrs()) <= PAPI_OK)  {
+        fprintf (stderr, "PAPI error getting number of available hardware counters!\n");
+        return 0;
+    }
+    fprintf(stdout, "done!\nThis system has %d available counters.\n\n", num_hwcntrs);
 
-    // // We will be using at most NUM_EVENTS counters
-    // if (num_hwcntrs >= NUM_EVENTS) {
-    //     num_hwcntrs = NUM_EVENTS;
-    // } else {
-    //     fprintf (stderr, "Error: there aren't enough counters to monitor %d events!\n", NUM_EVENTS);
-    //     return 0;
-    // }
+    // We will be using at most NUM_EVENTS counters
+    if (num_hwcntrs >= NUM_EVENTS) {
+        num_hwcntrs = NUM_EVENTS;
+    } else {
+        fprintf (stderr, "Error: there aren't enough counters to monitor %d events!\n", NUM_EVENTS);
+        return 0;
+    }
 
-    // if (PAPI_add_events(EventSet,Events,NUM_EVENTS) != PAPI_OK)  {
-    //     fprintf(stderr,"PAPI library add events error!\n");
-    //     return 0;
-    // }
+    if (PAPI_add_events(EventSet,Events,NUM_EVENTS) != PAPI_OK)  {
+        fprintf(stderr,"PAPI library add events error!\n");
+        return 0;
+    }
   
     
 
@@ -168,45 +168,45 @@ int main(){
     // // for process time running in user mode -> PAPI_get_virt_usec()
     // // real and virtual clock cycles can also be read using the equivalent
     // // PAPI_get[real|virt]_cyc()
-    // start_usec = PAPI_get_real_usec();
+    start_usec = PAPI_get_real_usec();
 
     // /* Start counting events */
-    // if (PAPI_start(EventSet) != PAPI_OK) {
-    //     fprintf (stderr, "PAPI error starting counters!\n");
-    //     return 0;
-    // }
+    if (PAPI_start(EventSet) != PAPI_OK) {
+        fprintf (stderr, "PAPI error starting counters!\n");
+        return 0;
+    }
 
     bucket_sort(vetor,max);
 
     
-    // /* Stop counting events */
-    // if (PAPI_stop(EventSet,values) != PAPI_OK) {
-    //     fprintf (stderr, "PAPI error stoping counters!\n");
-    //     return 0;
-    // }
+    /* Stop counting events */
+    if (PAPI_stop(EventSet,values) != PAPI_OK) {
+        fprintf (stderr, "PAPI error stoping counters!\n");
+        return 0;
+    }
 
-    // end_usec = PAPI_get_real_usec();
-    // fprintf (stdout, "done!\n");
+    end_usec = PAPI_get_real_usec();
+    fprintf (stdout, "done!\n");
 
-    // elapsed_usec = end_usec - start_usec;
+    elapsed_usec = end_usec - start_usec;
 
-    // if ((run==0) || (elapsed_usec < min_usec)) {
-    //     min_usec = elapsed_usec;
-    //     for (i=0 ; i< NUM_EVENTS ; i++) min_values[i] = values [i];
-    // }
+    if ((run==0) || (elapsed_usec < min_usec)) {
+        min_usec = elapsed_usec;
+        for (i=0 ; i< NUM_EVENTS ; i++) min_values[i] = values [i];
+    }
 
-    // fprintf (stdout,"\nWall clock time: %lld usecs\n", min_usec);
+    fprintf (stdout,"\nWall clock time: %lld usecs\n", min_usec);
 
-    // // output PAPI counters' values
-    // for (i=0 ; i< NUM_EVENTS ; i++) {
-    //     char EventCodeStr[PAPI_MAX_STR_LEN];
+    // output PAPI counters' values
+    for (i=0 ; i< NUM_EVENTS ; i++) {
+        char EventCodeStr[PAPI_MAX_STR_LEN];
 
-    //     if (PAPI_event_code_to_name(Events[i], EventCodeStr) == PAPI_OK) {
-    //         fprintf (stdout, "%s = %lld\n", EventCodeStr, min_values[i]);
-    //     } else {
-    //         fprintf (stdout, "PAPI UNKNOWN EVENT = %lld\n", min_values[i]);
-    //     }
-    // }
+        if (PAPI_event_code_to_name(Events[i], EventCodeStr) == PAPI_OK) {
+            fprintf (stdout, "%s = %lld\n", EventCodeStr, min_values[i]);
+        } else {
+            fprintf (stdout, "PAPI UNKNOWN EVENT = %lld\n", min_values[i]);
+        }
+    }
 
     
    
